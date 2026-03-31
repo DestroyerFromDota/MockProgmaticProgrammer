b1dd0b7c83958c06179437d1aac3d85c91f37495
DestroyerFromDota <101.00kg@gmail.com>
2026-03-31 23:14:58 +0300

    Обновлен контейнер менеджер и обновлены экзамены 1 и 2 под него

--- Control_Docker/manager.py
+++ Control_Docker/manager.py
@@ -1,15 +1,80 @@
 import docker
 
-client = docker.DockerClient(base_url='unix:///var/run/docker.sock')
 
-# Запустить контейнер с параметрами как в docker run
-container = client.containers.run(
-    'nginx-exam',                    # образ
-    detach=True,                     # -d
-    name='exam-student',             # --name
-    ports={
-        '22/tcp': 2222               # -p 2222:22
-    }
-)
+class ContainerManager:
+    """Класс для управления Docker контейнерами"""
 
-print(f"Контейнер {container.name} запущен с ID {container.id[:12]}")
+    def __init__(self, base_url='unix:///var/run/docker.sock'):
+        """Инициализация менеджера контейнеров"""
+        self.client = docker.DockerClient(base_url=base_url)
+        self.container = None
+        self.username = None
+        self.password = None
+
+    def run_container(self, image, name, port_mapping, username, password):
+        """
+        Запуск контейнера с заданными параметрами
+
+        Args:
+            image: имя образа
+            name: имя контейнера
+            port_mapping: словарь с маппингом портов
+            username: имя пользователя
+            password: пароль
+        """
+        self.username = username
+        self.password = password
+
+        self.container = self.client.containers.run(
+            image,
+            detach=True,
+            name=name,
+            ports=port_mapping,
+            environment={
+                'USERNAME': username,
+                'PASSWORD': password
+            }
+        )
+        return self.container
+
+    def print_container_info(self):
+        """Вывод информации о запущенном контейнере"""
+        if self.container:
+            print(f"Контейнер {self.container.name} запущен с ID {self.container.id[:12]}")
+            print(f'Логопасс для подключения:\n login: {self.username}\n password: {self.password}')
+        else:
+            print("Контейнер не запущен")
+
+    def stop_container(self):
+        """Остановка контейнера"""
+        if self.container:
+            self.container.stop()
+            print(f"Контейнер {self.container.name} остановлен")
+
+    def remove_container(self):
+        """Удаление контейнера"""
+        if self.container:
+            self.container.remove()
+            print(f"Контейнер {self.container.name} удален")
+
+
+# Использование класса
+if __name__ == "__main__":
+    # Создаем экземпляр менеджера
+    manager = ContainerManager()
+
+    # Запускаем контейнер
+    manager.run_container(
+        image='exam1',
+        name='exam-student',
+        port_mapping={'22/tcp': 2222},
+        username='qwerty',
+        password='1234'
+    )
+
+    # Выводим информацию
+    manager.print_container_info()
+
+    # При необходимости можно остановить и удалить контейнер
+    # manager.stop_container()
+    # manager.remove_container()
